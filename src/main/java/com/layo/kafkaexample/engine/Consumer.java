22ad4a33aed97947a0b37d8a38cf1da0641d2c66
ValentinShilin <valentin.shilin.de@gmail.com>
2023-06-20 20:35:13 +0200

    kafka spring

--- src/main/java/com/layo/kafkaexample/engine/Consumer.java
+++ src/main/java/com/layo/kafkaexample/engine/Consumer.java
@@ -9,3 +9,24 @@ import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
+
+@Service
+@ConditionalOnProperty(value = "example.kafka.consumer-enabled", havingValue = "true")
+public class Consumer {
+
+    private final Logger logger = LoggerFactory.getLogger(Producer.class);
+
+    @KafkaListener(topics = {"INPUT_DATA"})
+    public void consume(final @Payload String message,
+                        final @Header(KafkaHeaders.OFFSET) Integer offset,
+                        final @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String key,
+                        final @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
+                        final @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+                        final @Header(KafkaHeaders.RECEIVED_TIMESTAMP) long ts,
+                        final Acknowledgment acknowledgment
+    ) {
+        logger.info(String.format("#### -> Consumed message -> TIMESTAMP: %d\n%s\noffset: %d\nkey: %s\npartition: %d\ntopic: %s", ts, message, offset, key, partition, topic));
+        acknowledgment.acknowledge();
+    }
+}
+
